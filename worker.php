4bf05f30338479ca292bf7776bd0a1822bb76551
Hiroyuki Yamaoka <yamaoka@10mado.jp>
2015-02-19 20:34:16 +0900

    \Parallel\Prefork使うように

--- worker.php
+++ worker.php
@@ -1,5 +1,34 @@
 <?php
-while (true) {
+require_once __DIR__ . '/vendor/autoload.php';
+
+main();
+exit;
+
+function main()
+{
+    $pp = new \Parallel\Prefork([
+        'max_workers' => 2,
+        'trap_signals' => [
+            SIGHUP => SIGTERM,
+            SIGTERM => SIGTERM,
+        ],
+    ]);
+
+    while ($pp->signalReceived() !== SIGTERM) {
+        if ($pp->start()) {
+            continue;
+        }
+
+        work();
+
+        $pp->finish();
+    }
+
+    $pp->waitAllChildren();
+}
+
+function work()
+{
     echo 'Hello world at ' . date('Y-m-d H:i:s') . PHP_EOL;
     sleep(1);
 }
