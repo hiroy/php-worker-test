b70cf446723756f59b60be86f410632a66046da5
Hiroyuki Yamaoka <yamaoka@10mado.jp>
2015-02-20 14:22:16 +0900

    Update to use Clover\Worker latest version

--- src/App/Worker/TestWorker.php
+++ src/App/Worker/TestWorker.php
@@ -5,7 +5,7 @@ use Clover\Worker;
 
 class TestWorker extends Worker
 {
-    public function main()
+    public function main(array $args = [])
     {
         echo 'Hello world at ' . date('Y-m-d H:i:s') . PHP_EOL;
         sleep(1);
--- worker.php
+++ worker.php
@@ -2,5 +2,5 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 $worker = new App\Worker\TestWorker();
-$worker->start(['max_workers' => 3]);
+$worker->start([], ['max_workers' => 3]);
 
